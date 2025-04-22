d8d667549090ba744d789f23ee66fe9a9bbf5f15
dixitvij43015 <137907007+dixitvij43015@users.noreply.github.com>
2025-04-22 00:16:12 -0400

    adding interacive hello world
    git push was failing for some reason hence uploading.

---
+++ interactiveHelloWorld.py
@@ -0,0 +1,13 @@
+#Interactive hello world
+
+print("Hi, How are you!!")
+print ("what is your name ?") # asking name
+myName = input() # user input
+
+print ("hi, "+ myName)
+print ("length of your name is "+ str(len(myName)))
+
+print ("What is your age") # asking age
+myAge = input() # user input
+
+print ("your age will be "+ str(int(myAge)+1) + " in next 1 year")
